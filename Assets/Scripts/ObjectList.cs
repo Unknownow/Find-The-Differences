5535b317ed49556dbc1efe815d502dd26188a725
Unk <39058851+Unknownow@users.noreply.github.com>
2019-03-15 18:02:46 +0700

    update playscene

--- Assets/Scripts/ObjectList.cs
+++ Assets/Scripts/ObjectList.cs
@@ -7,22 +7,26 @@ public class ObjectList : MonoBehaviour {
     private int maxNumObjects = 5;
     [SerializeField]
     private int secondsDecreaseWhenChooseWrong = 20;
-    private GameManager gameManager;
     private int chosenObject = 0;
     private UILabel objectCount; 
 	// Use this for initialization
 	void Start () {
         chosenObject = 0;
         objectCount = GameObject.FindGameObjectWithTag("Object Count").GetComponent<UILabel>();
-        gameManager = gameObject.GetComponent<GameManager>();
         objectCount.text = "Found(0/" + maxNumObjects.ToString() + ")";
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (chosenObject >= maxNumObjects)
+        {
+            gameObject.GetComponent<NextLevelPanel>().openNextLevelPanel();
+            gameObject.GetComponent<GameManager>().stopTime(true);
+        }
+            
+    }
 
+    // called when the right object is selected
     public void selectRightObject()
     {
         Debug.Log("choose right!");
@@ -32,9 +36,11 @@ public class ObjectList : MonoBehaviour {
         objectCount.text = "Found(" + chosenObject.ToString() + "/" + maxNumObjects.ToString() + ")"; 
     }
 
+    // called when player choose wrong object
     public void selectWrongObject()
     {
         Debug.Log("choose wrong!");
-        gameManager.decreaseTimer(secondsDecreaseWhenChooseWrong);
+        gameObject.GetComponent<GameManager>().decreaseRemainingTimeBySecond(secondsDecreaseWhenChooseWrong);
     }
+
 }
