565ea993fbd1159b3b7aacdc36ad557695995c47
Unk <39058851+Unknownow@users.noreply.github.com>
2019-03-20 23:26:20 +0700

    Menu Level Selection Update

---
+++ Assets/Scripts/Scene_Playing/PausePanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePanel : MonoBehaviour {
+    private GameObject _pausePanel;
+    private TimeManager _timeManager;
+
+	// Use this for initialization
+	void Start () {
+        _pausePanel = GameObject.FindGameObjectWithTag("Pause Panel");
+        _pausePanel.SetActive(false);
+        _timeManager = gameObject.GetComponent<TimeManager>();
+	}
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _pausePanel.SetActive(!_pausePanel.activeSelf);
+        }
+    }
+
+    //pause function. 
+    public void pauseButton()
+    {
+        _pausePanel.SetActive(true);
+        _pausePanel.transform.GetChild(1).GetComponent<TweenAlpha>().PlayForward();
+        _timeManager.stopTime(true);
+    }
+
+
+    //resume function
+    public void resumeButton()
+    {
+        _timeManager.stopTime(false);
+    }
+}
