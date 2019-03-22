055246fe7a217b8dfa97df79088b6d77dad06dad
Unk <39058851+Unknownow@users.noreply.github.com>
2019-03-22 17:48:26 +0700

    Add shop panel and bug fixed

---
+++ Assets/Scripts/Scene_Main Menu/CaseSelection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseSelection : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _levelSelectionPanel;
+    [SerializeField]
+    private int _caseIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelSelectionPanel = GameObject.FindGameObjectWithTag("Level Selection Panel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onCaseSelectionClick()
+    {
+        GameObject backGround = _levelSelectionPanel.transform.GetChild(0).gameObject;
+        backGround.SetActive(true);
+        GameObject levelPanel = _levelSelectionPanel.transform.GetChild(_caseIndex + 1).gameObject;
+        levelPanel.SetActive(true);
+        GameObject backButton = _levelSelectionPanel.transform.GetChild(1).gameObject;
+        backButton.SetActive(true);
+        backGround.GetComponent<TweenAlpha>().PlayForward();
+        levelPanel.GetComponent<TweenAlpha>().PlayForward();
+        backButton.GetComponent<TweenAlpha>().PlayForward();
+        GameObject.FindGameObjectWithTag("Menu Manager").GetComponent<MenuManager>().setCurrentCase(_caseIndex);
+    }
+}
---
+++ Assets/Scripts/Scene_Main Menu/Gold Management/IAPButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAPButtons : MonoBehaviour
+{
+    [SerializeField]
+    private int _goldAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void watchAds()
+    {
+        Debug.Log("Watching ads...");
+    }
+
+    public void onBuyGoldButtonClick(int amount = 0)
+    {
+        if (amount == 0)
+            amount = _goldAmount;
+        Debug.Log("Buy " + amount + " golds");
+        PlayerPrefs.SetInt("Gold", PlayerPrefs.GetInt("Gold") + amount);
+    }
+
+}
