12db32de935225f7bcd5db8e7337e25e86a98539
Unk <39058851+Unknownow@users.noreply.github.com>
2019-04-09 07:50:42 +0700

    Bug Fixed

---
+++ Assets/Scripts/Scene_Main Menu/ButtonManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+/*
+ * this class is used to manage all the button in the main menu
+*/
+
+public class ButtonManager : MonoBehaviour
+{
+    private GameObject _levelSelectionPanel; //instance of level selection panel
+    private PlayerProperties _playerProperties; //instance of player properties
+    private GameObject _caseSelectionPanel;
+    private UICenterOnChild _centerOnChild;
+
+
+    //Find all the game object needed.
+    private void Awake()
+    {
+
+        _playerProperties = GameObject.FindGameObjectWithTag("Player Properties").GetComponent<PlayerProperties>();
+        _levelSelectionPanel = GameObject.FindGameObjectWithTag("Level Selection Panel");
+        _caseSelectionPanel = GameObject.FindGameObjectWithTag("Case Selection Panel");
+        _centerOnChild = _caseSelectionPanel.GetComponent<UICenterOnChild>();
+    }
+
+
+
+    private void Start()
+    {
+        int i = 0;
+
+        //turn off all level selection panel
+        while (i < _levelSelectionPanel.transform.childCount)
+        {
+            _levelSelectionPanel.transform.GetChild(i).gameObject.SetActive(false);
+            i++;
+        }
+
+        //this will show the level selection panel when use that button on ingame scene
+        if (_playerProperties.getIsBackToLevelSelection())
+        {
+            GameObject.FindGameObjectWithTag("Case Selection Panel").transform.GetChild(_playerProperties.getCurrentCase() - 1).GetComponent<CaseSelectionButton>().onCaseSelectionClick();
+            _playerProperties.setIsBackToLevelSelection(false);
+            return;
+        }
+  
+        if (_playerProperties.getIsFinishCase())
+        {
+            _centerOnChild.enabled = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _caseSelectionPanel.GetComponent<UICenterOnChild>().CenterOn(_caseSelectionPanel.transform.GetChild(4));
+        }
+        if (_playerProperties.getIsFinishCase())
+        {
+            onFinishCase();
+        }
+    }
+
+
+    //call when use back from level selection
+    public void onBackFromLevelSelectionClick()
+    {
+        _levelSelectionPanel.transform.GetChild(_playerProperties.getCurrentCase() + 1).GetComponent<TweenAlpha>().PlayReverse();
+        _levelSelectionPanel.GetComponent<TweenAlpha>().PlayReverse();
+    }
+
+    public void onFinishCase()
+    {
+        //Debug.Log("Case: " + _playerProperties.getCurrentCase());
+        if(_playerProperties.canCenterOnChild())
+            _centerOnChild.CenterOn(_caseSelectionPanel.transform.GetChild(_playerProperties.getCurrentCase()));
+        if (_centerOnChild.onCenter == null)
+        {
+            _playerProperties.setIsFinishCase(false);
+            _centerOnChild.enabled = false;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Scene_Main Menu/Gold Management/PlayerLivesManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeStage.AntiCheat.Detectors;
+using System;
+
+public class PlayerLivesManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _maxLives = 5;
+    private int _currentLives;
+    private bool _isTimeCheating = false;
+    private TimeCheatingDetector timeDetector;
+
+    private int _month;
+    private int _day;
+    private int _year;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeDetector = gameObject.GetComponent<TimeCheatingDetector>();
+        Debug.Log(timeDetector.ForceCheck());
+        Debug.Log(DateTime.Today);
+        if (_isTimeCheating)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void firstTimeSetup()
+    {
+        _day = DateTime.Today.Day;
+        _month = DateTime.Today.Month;
+        _year = DateTime.Today.Year;
+        string temp = _day + "/" + _month + "/" + _year;
+        PlayerPrefs.SetString("Time", temp);
+    }
+
+    public void saveLives()
+    {
+        PlayerPrefs.SetInt("Lives", _currentLives);
+    }
+
+    public void getLives()
+    {
+        _currentLives = PlayerPrefs.GetInt("Lives");
+    }
+
+    public bool isPlayable()
+    {
+        if (PlayerPrefs.GetInt("Lives") <= 0)
+            return false;
+        return true;
+    }
+
+    public void onTimeCheating()
+    {
+        _isTimeCheating = true;
+    }
+
+    private bool checkResetLives()
+    {
+
+    }
+}
