d8ce0970ba4282aacc2282b7d10b8e665512619c
Unk <39058851+Unknownow@users.noreply.github.com>
2019-04-10 17:56:30 +0700

    UI update
    finish UI in Gameplay
    Add back to main menu popup
    add wrong click mark

---
+++ Assets/Scripts/Scene_Playing/Managers/ChangeColorRetry.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorRetry : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isRetry = false;
+
+    private LivesAndDailyManager _livesManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _livesManager = GameObject.FindGameObjectWithTag("Player Properties").GetComponent<LivesAndDailyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isRetry)
+        {
+            if(!_livesManager.isPlayable())
+            {
+                gameObject.GetComponent<UISprite>().alpha = 1;
+                return;
+            }
+            gameObject.GetComponent<UISprite>().alpha = 0;
+            return;
+        }
+        if (!_livesManager.isPlayable())
+        {
+            gameObject.GetComponent<UISprite>().color = Color.gray;
+            gameObject.GetComponent<UIButton>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<UISprite>().color = Color.white;
+            gameObject.GetComponent<UIButton>().enabled = true;
+        }
+    }
+}
